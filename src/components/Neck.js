611c22d9896d0257c7a35e6a1158ec3d26281a78
zuo132 <xiez@tcd.ie>
2021-11-22 11:35:13 +0000

    Handle string length change

--- src/components/Neck.js
+++ src/components/Neck.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Line } from 'react-konva';
+import { Line, Shape } from 'react-konva';
 import { stringX, stringY, getQBezierValue } from '../utils';
 
 const Neck = ({ start, end, yOffset }) => {
@@ -19,7 +19,8 @@ const Neck = ({ start, end, yOffset }) => {
             points={[
               start.x,
               shape === 'Straight' ? start.y : start.y + 50,
-              ...samplePoints(
+              ...topNeckPoints(
+                strings,
                 defaultStringLengths,
                 stringY(-4, yOffset),
                 control.y,
@@ -47,7 +48,8 @@ const Neck = ({ start, end, yOffset }) => {
             points={[
               start.x,
               start.y + 50,
-              ...samplePoints(
+              ...bottomNeckPoints(
+                strings,
                 defaultStringLengths,
                 stringY(-4, yOffset),
                 control.y,
@@ -63,6 +65,67 @@ const Neck = ({ start, end, yOffset }) => {
             lineCap='round'
             listening={false}
           />
+
+          {/* {strings.map((string, index) => {
+            const points = neckPoints(
+              strings.map((string) => string.length),
+              stringY(-4, yOffset),
+              control.y,
+              end.y,
+              stringSpacing,
+              10
+            );
+
+            if (string.length - defaultStringLengths[index] > 50) {
+              return (
+                <Shape
+                  key={string.id}
+                  stroke='black'
+                  strokeWidth={3}
+                  lineCap='round'
+                  sceneFunc={(context, shape) => {
+                    context.beginPath();
+                    context.moveTo(points[index - 1][0], points[index - 1][1]);
+                    context.lineTo(points[index][0], points[index][1]);
+                    context.moveTo(points[index + 1][0], points[index + 1][1]);
+                    context.fillStrokeShape(shape);
+                  }}
+                />
+              );
+            }
+
+            if (string.length - defaultStringLengths[index] < -10) {
+              return (
+                <Shape
+                  key={string.id}
+                  stroke='black'
+                  strokeWidth={3}
+                  lineCap='round'
+                  lineJoin='round'
+                  sceneFunc={(context, shape) => {
+                    context.beginPath();
+                    context.moveTo(
+                      index !== 0 ? points[index - 1][0] : points[index + 1][0] - 20,
+                      index !== 0 ? points[index - 1][1] : points[index + 1][1]
+                    );
+                    context.lineTo(points[index][0] - 5, points[index][1]);
+                    context.lineTo(points[index][0] + 5, points[index][1]);
+                    context.lineTo(
+                      index !== defaultStringLengths.length - 1
+                        ? points[index + 1][0]
+                        : points[index - 1][0] + 20,
+                      index !== defaultStringLengths.length - 1
+                        ? points[index + 1][1]
+                        : points[index - 1][1]
+                    );
+                    context.fillStrokeShape(shape);
+                  }}
+                />
+              );
+            }
+
+            return null;
+          })} */}
         </>
       )}
     </>
@@ -86,6 +149,54 @@ const samplePoints = (stringLengths, start, control, end, stringSpacing, offset)
 
   points.forEach((point, index) => {
     if (index % 6 === 0) {
+      samples.push(point);
+    }
+  });
+
+  return samples.flat();
+};
+
+const bottomNeckPoints = (strings, stringLengths, start, control, end, stringSpacing, offset) => {
+  const points = neckPoints(
+    strings.map((string) => string.length),
+    start,
+    control,
+    end,
+    stringSpacing,
+    offset
+  );
+  const defaultPoints = neckPoints(stringLengths, start, control, end, stringSpacing, offset);
+  const samples = [];
+
+  points.forEach((point, index) => {
+    const lengthDifference = strings[index].length - stringLengths[index];
+    if (index % 6 === 0 && lengthDifference > -10) {
+      samples.push(defaultPoints[index]);
+    } else if (index % 6 === 0 || lengthDifference < -10) {
+      samples.push(point);
+    }
+  });
+
+  return samples.flat();
+};
+
+const topNeckPoints = (strings, stringLengths, start, control, end, stringSpacing, offset) => {
+  const points = neckPoints(
+    strings.map((string) => string.length),
+    start,
+    control,
+    end,
+    stringSpacing,
+    offset
+  );
+  const defaultPoints = neckPoints(stringLengths, start, control, end, stringSpacing, offset);
+  const samples = [];
+
+  points.forEach((point, index) => {
+    const lengthDifference = strings[index].length - stringLengths[index];
+    if (index % 6 === 0 && lengthDifference < 50) {
+      samples.push(defaultPoints[index]);
+    } else if (index % 6 === 0 || lengthDifference > 50) {
       samples.push(point);
     }
   });
